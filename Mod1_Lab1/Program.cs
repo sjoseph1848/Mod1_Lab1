c0bacb6334844ac28ce61011ff6db4037d0180a2
Sam <sjoseph1848@gmail.com>
2017-11-15 00:06:02 -0600

    Updates to Class

--- Mod1_Lab1/Program.cs
+++ Mod1_Lab1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Mod1_Lab1
 {
@@ -10,6 +11,27 @@ namespace Mod1_Lab1
     {
         static void Main(string[] args)
         {
+            //Instantiating an object of Car() Class by using the Type Inference
+            //called Car1
+            var Car1 = new Car();
+            //using dot notation to call members on Car1
+            Car1.Color = "White";
+            Car1.Year = 2010;
+            Car1.Mileage = 11000;
+
+            WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}," +
+                $" and had {Car1.Mileage} miles on it.");
+
+            ReadKey();
         }
+    }
+    //Declaring the Car() Class
+    //This class has 3 properties: color, year, and Mileage
+    public class Car
+    {
+        //Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
     }
 }
